43b7c3829ca1d7a54f3c224f75543f0254a26051
china-abo <z_mb@qq.com>
2017-02-17 01:15:14 +0800

    优化代码
    先把页面下载下来，再分析。

--- spaider.js
+++ spaider.js
@@ -1,8 +1,10 @@
 // 这一行是套路, 给 node.js 用的
-// 如果没有这一行, 就没办法使用一些 let var 这样的特性
+// 如果没有这一行, 就没办法使用一些 var var 这样的特性
 "use strict"
 var request = require('request')
 var cheerio = require('cheerio')
+var fs = require('fs')
+
 /*
 本文件需要安装两个基本的库
 request 用于下载网页
@@ -22,6 +24,7 @@ var log = function() {
     console.log.apply(console, arguments)
 }
 var movies = []
+var years = []
 var movieFromDiv = function(div) {
     // 这个函数来从一个电影 div 里面读取电影信息
     var movie = new Movie()
@@ -34,20 +37,18 @@ var movieFromDiv = function(div) {
     movie.quote = e('.inq').text()
     //垃圾豆瓣 年份没有标签，和导演演员放一块。有的还有2个年份。
     movie.year = e('.info .bd p').text().match(/\d+/g).join('').slice(0, 4)
+    //提取电影年份
+    years.push(movie.year)
     var pic = e('.pic')
     movie.ranking = pic.find('em').text()
     // 元素的属性用 .attr('属性名') 确定
     movie.coverUrl = pic.find('img').attr('src')
     movie.people = e('.star').find('span').last().text().match(/\d+/g).join('')
-    console.log(movie.name)
     return movie
 }
-var saveMovie = function(movies) {
+var save = function(data, path) {
     // 这个函数用来把一个保存了所有电影对象的数组保存到文件中
-    var fs = require('fs')
-    var path = 'douban.txt'
-    var s = JSON.stringify(movies, null, 2)
-    fs.writeFile(path, s, function(error) {
+    fs.writeFile(path, data, function(error) {
         if (error !== null) {
             log('*** 写入文件错误', error)
         } else {
@@ -55,44 +56,120 @@ var saveMovie = function(movies) {
         }
     })
 }
+var saveMovie = function(movies) {
+    // 这个函数用来把一个保存了所有电影对象的数组保存到文件中
+    var data = JSON.stringify(movies, null, 2)
+    var path = 'douban.txt'
+    save(data, path)
+}
+//保存年份
+var saveYear = function(year) {
+    var path = 'year.txt'
+    save(year, path)
+}
+var writeToFile = function(path, data) {
+    fs.writeFile(path, data, function(error) {
+        if (error != null) {
+            log('--- 写入成功', path)
+        } else {
+            log('*** 写入失败', path)
+        }
+    })
+}
+var cachedUrl = function(url, callback) {
+    // 先生成对应的文件
+    var path = url.split('/').join('-').split(':').join('-') + '.html'
+    // 先尝试去硬盘中读取这个 url 对应的文件
+    fs.readFile(path, function(err, data) {
+        if (err != null) {
+            // 读取这个文件失败
+            // 读不到的话说明是第一次请求，那么就使用 request
+            request(url, function(error, response, body) {
+                // 下载好了之后，保存到本地文件
+                // TODO, 应该下载成功之后再保存
+                writeToFile(path, body)
+                callback(error, response, body)
+            })
+        } else {
+            log('读取到缓存的页面', path)
+            // 读取到，说明已经下载过了，我们讲直接读取硬盘上的文件
+            var response = {
+                statusCode: 200
+            }
+            callback(null, response, data)
+        }
+    })
+}
 var moviesFromUrl = function(url) {
     // request 从一个 url 下载数据并调用回调函数
-    request(url, function(error, response, body) {
+    // request(url, function(error, response, body) {
+    //     // 回调函数的三个参数分别是  错误, 响应, 响应数据
+    //     // 检查请求是否成功, statusCode 200 是成功的代码
+    //     if (error === null && response.statusCode == 200) {
+    //         // cheerio.load 用字符串作为参数返回一个可以查询的特殊对象
+    //         var e = cheerio.load(body)
+    //         // 查询对象的查询语法和 DOM API 中的 querySelector 一样
+    //         var movieDivs = e('.item')
+    //         for (var i = 0; i < movieDivs.length; i++) {
+    //             var element = movieDivs[i]
+    //             // 获取 div 的元素并且用 movieFromDiv 解析
+    //             // 然后加入 movies 数组中
+    //             var div = e(element).html()
+    //             var m = movieFromDiv(div)
+    //             movies.push(m)
+    //         }
+    //         // 保存 movies 数组到文件中
+    //         saveMovie(movies)
+    //         saveYear(years)
+    //     } else {
+    //         log('*** ERROR 请求失败 ', error)
+    //     }
+    // })
+    cachedUrl(url, function(error, response, body) {
         // 回调函数的三个参数分别是  错误, 响应, 响应数据
         // 检查请求是否成功, statusCode 200 是成功的代码
         if (error === null && response.statusCode == 200) {
-            // cheerio.load 用字符串作为参数返回一个可以查询的特殊对象
             var e = cheerio.load(body)
             // 查询对象的查询语法和 DOM API 中的 querySelector 一样
             var movieDivs = e('.item')
-            for (let i = 0; i < movieDivs.length; i++) {
-                let element = movieDivs[i]
+            for (var i = 0; i < movieDivs.length; i++) {
+                var element = movieDivs[i]
                 // 获取 div 的元素并且用 movieFromDiv 解析
                 // 然后加入 movies 数组中
                 var div = e(element).html()
                 var m = movieFromDiv(div)
+                var y = m.year
+                years.push(y)
                 movies.push(m)
             }
             // 保存 movies 数组到文件中
             saveMovie(movies)
+            saveYear(years)
         } else {
             log('*** ERROR 请求失败 ', error)
         }
     })
 }
-var __main = function() {
-    // 这是主函数
-    // 下载网页, 解析出电影信息, 保存到文件
+var pa = function() {
     var i = 0
-    var pa = setInterval(function() {
+    var wait = setInterval(function() {
         var url = 'https://movie.douban.com/top250?start=' + i
         moviesFromUrl(url)
         i = i + 25
         if (i > 225) {
-            clearInterval(pa)
+            clearInterval(wait)
         }
     }, 1000)
-
+}
+var __main = function() {
+    // 下载网页,解析出电影信息,保存到文件
+    var i = 0
+    while (i < 250) {
+        var url = 'https://movie.douban.com/top250?start=' + i
+        moviesFromUrl(url)
+        i = i + 25
+    }
+    // pa()
 }
 
 // 程序开始的主函数
